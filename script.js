17a42a76e8abd7129c61523868f65076fb507342
akaushik456 <kaushikaman239@gmail.com>
2024-03-15 20:18:41 +0530

    add-time

--- script.js
+++ script.js
@@ -28,3 +28,11 @@ else{
     localStorage.setItem("theme", "light");
 }
 
+
+
+let time = document.getElementById("current-time");
+
+setInterval(() => {
+    let d = new Date();
+    time.innerHTML = d.toLocaleTimeString();
+},1000);
